e73589e10ee91ee3158d7097b0bfc969fd8f7cae
Yaroslav Prystash <prystash.yaroslav@gmail.com>
2023-05-01 20:43:15 +0300

    first commit

---
+++ ITstep8/index.js
@@ -0,0 +1,14 @@
+let c
+alert('Задумайте число');
+alert('Помножте задумане число на 2');
+alert('Додайте до отриманого результату 7');
+let number = prompt('Виведіть що вийшло');
+function myName() {
+    const a = 7;
+    const b = 2;
+   c = number - a;  
+    return alert('Ви задумали число ' + c / b)
+}
+myName()
+
+
